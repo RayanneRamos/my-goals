fdf34f5d672b4262aea3cd7963481b88ec7d9283
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-03-07 16:05:41 -0300

    criando o método de mostrar os produtos

--- src/database/useGoalRepository.ts
+++ src/database/useGoalRepository.ts
@@ -32,20 +32,34 @@ export function useGoalsRepository() {
 
   function all() {
     try {
-      return database.getAllSync<GoalResposeDatabaseProps>(
-        ` SELECT g.id, g.name, g.total, COALESCE(SUM(t.amount), 0) AS current 
+      return database.getAllSync<GoalResposeDatabaseProps>(` 
+        SELECT g.id, g.name, g.total, COALESCE(SUM(t.amount), 0) AS current 
         FROM goals AS g 
         LEFT JOIN transactions t ON t.goal_id = g.id
         GROUP BY g.id, g.name, g.total
-        `
-      );
+        `);
     } catch (error) {
       throw error;
     }
   }
 
+  function show(id: number) {
+    const statement = database.prepareSync(` 
+      SELECT g.id, g.name, g.total, COALESCE(SUM(t.amount), 0) AS current 
+      FROM goals AS g 
+      LEFT JOIN transactions t ON t.goal_id = g.id
+      WHERE g.id = $id
+      GROUP BY g.id, g.name, g.total
+    `);
+
+    const result = statement.executeSync<GoalResposeDatabaseProps>({ $id: id });
+
+    return result.getFirstSync();
+  }
+
   return {
     create,
     all,
+    show,
   };
 }
