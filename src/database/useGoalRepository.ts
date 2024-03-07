6295c719ab0b2e417bbfc388cdf1ed8236af95cd
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-03-07 15:52:33 -0300

    criando o método de listar tudo

--- src/database/useGoalRepository.ts
+++ src/database/useGoalRepository.ts
@@ -2,24 +2,50 @@ import { useSQLiteContext } from "expo-sqlite/next";
 
 export type GoalCreateDatabaseProps = {
   name: string;
-  total: string;
+  total: number;
+};
+
+export type GoalResposeDatabaseProps = {
+  id: string;
+  name: string;
+  total: number;
+  current: number;
 };
 
 export function useGoalsRepository() {
   const database = useSQLiteContext();
 
   function create(goal: GoalCreateDatabaseProps) {
-    const statement = database.prepareSync(
-      "INSERT INTO goals (name, total) VALUES ($name, $total)"
-    );
+    try {
+      const statement = database.prepareSync(
+        "INSERT INTO goals (name, total) VALUES ($name, $total)"
+      );
 
-    statement.executeSync({
-      $name: goal.name,
-      $total: goal.total,
-    });
+      statement.executeSync({
+        $name: goal.name,
+        $total: goal.total,
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  function all() {
+    try {
+      return database.getAllSync<GoalResposeDatabaseProps>(
+        ` SELECT g.id, g.name, g.total, COALESCE(SUM(t.amount), 0) AS current 
+        FROM goals AS g 
+        LEFT JOIN transactions t ON t.goal_id = g.id
+        GROUP BY g.id, g.name, g.total
+        `
+      );
+    } catch (error) {
+      throw error;
+    }
   }
 
   return {
     create,
+    all,
   };
 }
